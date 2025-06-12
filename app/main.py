8a7b3bb9c88548f3cc0ebb59a7affb2f905d2056
Carlos Eduardo Sousa Silva <carloseduardosousas18@gmail.com>
2025-06-12 00:12:05 -0300

    Inclusão de comentários

--- app/main.py
+++ app/main.py
@@ -1,17 +1,17 @@
-from fastapi import FastAPI 
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI # Importando o FastAPI para criar a aplicação
+from fastapi.middleware.cors import CORSMiddleware # Importando o middleware CORS para permitir requisições de diferentes origens
 from app.routers import user_router # importa o router
 
-app = FastAPI(
-    title="Usalista",
-    description="Uma aplicação para gerenciar listas de compras com autenticação de usuários.",
+app = FastAPI(  # Cria uma instância do FastAPI, que é a aplicação principal 
+    title="Usalista", # Título da API, que será exibido na documentação Swagger
+    description="Uma aplicação para gerenciar listas de compras com autenticação de usuários.", # Descrição da API, que será exibida na documentação Swagger
     version="1.0.0"
 )
 
 app.include_router(user_router.router) # Inclui no app / Inclui o router de usuários
 
 app.add_middleware( # Adiciona o middleware CORS para permitir requisições de diferentes origens
-    CORSMiddleware,
+    CORSMiddleware, 
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
