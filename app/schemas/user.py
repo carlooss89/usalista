c950206aaef04673bea519b6214b539964df5c50
Carlos Eduardo Sousa Silva <carloseduardosousas18@gmail.com>
2025-06-11 21:17:23 -0300

    ajustando os espaços e comentários

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,7 +2,7 @@
 
 from pydantic import BaseModel, EmailStr # Importando BaseModel e EmailStr do Pydantic para validação de dados
 
-# Schema usado para criar um novo usuário
+# Schema usado para criar um novo usuário (entrada de dados)
 class UserCreate(BaseModel):
     nome: str
     email: EmailStr
